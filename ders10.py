c4702d3fc797835bd36f16e8aaea091cc1f008a0
Ubuntu <sbesinci@ubuntu.empra4vdrldehkkuhwt3fipuec.bx.internal.cloudapp.net>
2019-08-10 01:36:13 +0000

    delete files

--- ders10.py
+++
@@ -1,7 +0,0 @@
-while True:
-     parola = input("Lütfen bir parola belirleyiniz:")
-     if len(parola) < 5:
-         print("Parola 5 karakterden az olmamalý!")
-     else:
-         print("Parolanýz belirlendi!")
-         break
--- randevu.py
+++
@@ -1,5 +0,0 @@
-tarih = "02.01.2012"
-gün = "Pazartesi"
-vakit = "öğleden sonra"
-
-print(tarih, gün, vakit, "buluşalım", end=".\n")
