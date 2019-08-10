e984fcbb880e0ef9cffe1043e44a6d26dadee877
Ubuntu <sbesinci@ubuntu.empra4vdrldehkkuhwt3fipuec.bx.internal.cloudapp.net>
2019-08-10 00:32:35 +0000

    finish try.catch

--- 15HataYakalama.py
+++ 15HataYakalama.py
@@ -1,7 +1,7 @@
 # Hata Yakalama
 
 # try... except...
-
+"""
 while True:
     ilk_sayı    = input("ilk sayı: ")
     ikinci_sayı = input("ikinci sayı: ")
@@ -10,5 +10,15 @@ while True:
         sayı2 = int(ikinci_sayı)
         print(sayı1, "/", sayı2, "=", sayı1 / sayı2)
         break
-    except ValueError:
+    except (ValueError, ZeroDivisionError) as e:
+        print("Bir hata oluştu!")
+        print("Error Message:", e)
+'''
+    except ValueError as e:
         print("Lütfen sadece sayı girin!")
+        print("Error Message:", e)
+    except ZeroDivisionError as e:
+        print("Lütfen böleni sıfır girmeyin!")
+        print("Error Message:", e)
+'''
+"""
